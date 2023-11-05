bb7a9ac2f76cf54fc28c8ceb9947c6eb9c8f3604
xxMelkorxx <xxxserborxxx@gmail.com>
2023-11-05 16:04:54 +0300

    chore: intermediate save

--- gold-codes/SignalGenerator.cs
+++ gold-codes/SignalGenerator.cs
@@ -85,6 +85,63 @@ public class SignalGenerator
     }
     
     /// <summary>
+    /// Генерация случайного числа с нормальным распределением.
+    /// </summary>
+    /// <param name="min">Минимальное число (левая граница)</param>
+    /// <param name="max">Максимальное число (правая граница)</param>
+    /// <param name="n">Количество случайных чисел, которые необходимо суммировать для достижения нормального распределения</param>
+    /// <returns>Случайное нормально распределённое число</returns>
+    private static double GetNormalRandom(double min, double max, int n = 12)
+    {
+        var rnd = new Random(Guid.NewGuid().GetHashCode());
+        var sum = 0d;
+        for (var i = 0; i < n; i++)
+            sum += rnd.NextDouble() * (max - min) + min;
+        return sum / n;
+    }
+
+    /// <summary>
+    /// Генерация отнормированного белого шума.
+    /// </summary>
+    /// <param name="countNumbers">Число отсчётов</param>
+    /// <param name="energySignal">Энергия сигнала, на который накладывается шум</param>
+    /// <param name="snrDb">Уровень шума в дБ</param>
+    /// <returns></returns>
+    private static IEnumerable<double> GenerateNoise(int countNumbers, double energySignal, double snrDb)
+    {
+        var noise = new List<double>();
+        for (var i = 0; i < countNumbers; i++)
+            noise.Add(GetNormalRandom(-1d, 1d));
+
+        // Нормировка шума.
+        var snr = Math.Pow(10, -snrDb / 10);
+        var norm = Math.Sqrt(snr * energySignal / noise.Sum(y => y * y));
+
+        return noise.Select(y => y * norm).ToList();
+    }
+
+
+    /// <summary>
+    /// Наложить шум на сигнал.
+    /// </summary>
+    /// <param name="snrDb"></param>
+    /// <returns></returns>
+    public void MakeNoise(double snrDb)
+    {
+        // Наложение шума на искомый сигнал.
+        // desiredSignal = desiredSignal.Zip(
+        //         GenerateNoise(researchedSignal.Count, researchedSignal.Sum(p => p.Y * p.Y), snrDb),
+        //         (p, n) => new PointD(p.X, p.Y + n))
+        //     .ToList();
+
+        // Наложение шума на исследуемый сигнал.
+        // researchedSignal = researchedSignal.Zip(
+        //         GenerateNoise(researchedSignal.Count, researchedSignal.Sum(p => p.Y * p.Y), snrDb),
+        //         (p, n) => new PointD(p.X, p.Y + n))
+        //     .ToList();
+    }
+    
+    /// <summary>
     /// Генерация битовой последовательности.
     /// </summary>
     /// <param name="countBits">кол-во битов последовательности</param>
